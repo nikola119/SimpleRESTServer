0e0c0aa3e77a321a7db4e255275a408ba383f6ef
nikola.dodic <nikola.dodic@intelisale.com>
2018-01-11 11:45:21 +0100

    First Commit

---
+++ SimpleRESTServer/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRESTServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController
+    {
+        public PersonController()
+        {
+
+        }
+        [HttpGet()]
+        public Person GetPersonData()
+        {
+            Person john = new Person("John", "Doe");
+            return john;
+        }
+    }
+}
---
+++ SimpleRESTServer/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRESTServer
+{
+    public class Person
+    {
+        private string ime;
+        public string Ime { get; set; }
+        private string prezime;
+        public string Prezime { get; set; }
+
+        public Person(string ime, string prezime)
+        {
+            this.ime = ime; this.prezime = prezime;
+        }
+    }
+}
